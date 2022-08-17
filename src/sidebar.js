fc268bc76b6b29b71feed85b8ccee8b5eba0d462
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-17 17:53:15 -0600

    sidebar buttons are done

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,16 +1,46 @@
 /* Creates the side bar*/
 let loadSidebar= (content) => {
-    createSidebarContainer(content)
+    let sidebarContainer = createSidebarContainer(content)
+    createSidebar(sidebarContainer)
 }
 
 let createSidebarContainer = (content) => {
     let sidebarContainer = document.createElement('div')
     sidebarContainer.classList.add('sidebarContainer')
-    sidebarContainer.textContent = 'sadasd'
 
     content.appendChild(sidebarContainer)
+
+    return sidebarContainer
 }
 
+let createSidebar = (container) => {
+    
+    let itemNames = ['home','today','week','month','projects']
+
+    itemNames.forEach(item => {
+        createSidebarItem(item,container)
+    });
+
+
+}
+
+
+let createSidebarItem = (name,container) =>{
+    
+    let button = document.createElement('button')
+    button.textContent = capitalizeFirstLetter(name)
+    button.setAttribute('id',`${name}Button`)
+    button.classList.add(`sidebarButton`)
+
+    container.appendChild(button)
+
+}
+
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export{
     loadSidebar
