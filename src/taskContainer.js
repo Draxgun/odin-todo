f5daaa5c4ccb241a60631fd6541c0bd083d60491
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-17 12:43:24 -0600

    main page structure to commit and edit

--- src/taskContainer.js
+++ src/taskContainer.js
@@ -1,14 +1,32 @@
 let loadTaskSection = (content) => {
-    createTaskContainer(content)
+    let main = createTaskSection()
+    createTaskContainer(main)
+    createFooter(main)
+    content.appendChild(main)
 }
 
-let createTaskContainer = (content) => {
-    let taskContainer = document.createElement('div')
-    taskContainer.classList.add('taskContainer')
-    taskContainer.textContent ='asdas'
+let createTaskSection = () => {
+    let taskSectionContainer = document.createElement('div')
+    taskSectionContainer.classList.add('taskSectionContainer')
+    taskSectionContainer.textContent ='asdas'
 
-    content.appendChild(taskContainer)
+    return taskSectionContainer
 }
+
+let createTaskContainer = (taskSectionContainer) => {
+    let tasksContainer = document.createElement('div')
+    tasksContainer.classList.add('taskContainer')
+
+    taskSectionContainer.appendChild(tasksContainer)
+}
+
+let createFooter = (taskSectionContainer) => {
+    let footer = document.createElement('div')
+    footer.classList.add('footer')
+    footer.textContent = 'A project by Alejandro Banuelos'
+    taskSectionContainer.appendChild(footer)
+}
+
 
 export{
     loadTaskSection
