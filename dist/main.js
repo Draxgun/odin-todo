87592045b4bd95667e882fd67e42a94532305684
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-16 21:00:07 -0600

    project structure mainly done

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hola')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFpbi8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaG9sYScpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
